4b094e7ea4331aefdcfb5075889effee50dc70dd
Dobber Peas <dobberpe@le-c4.nov.21-school.ru>
2023-12-25 17:29:46 +0300

    -s works

--- src/cat/cat.c
+++ src/cat/cat.c
@@ -25,10 +25,10 @@ cat_args* get_options(int argc, char** argv) {
         else if (option == 'e' || option == 'E') {
             cat->endl = true;
             if (option == 'e') cat->nonprint = true;
-        } else if (option == 'n') cat->num_nonblank = true;
-        else if (option == 's') cat->num_nonblank = true;
+        } else if (option == 'n') cat->num = true;
+        else if (option == 's') cat->squeeze = true;
         else if (option == 't' || option == 'T') {
-            cat->num_nonblank = true;
+            cat->tab = true;
             if (option == 't') cat->nonprint = true;
         } else fail = true;
     }
@@ -70,6 +70,8 @@ void get_files(int argc, char** argv, cat_args* cat) {
 void print_file(cat_args* cat) {
     char* line = NULL;
     int len = 0;
+    bool right_after_blank = false;
+
     for (int i = 0; i < cat->f_size; ++i) {
         FILE* file = fopen(cat->files[i], "r");
         if (file) {
@@ -81,17 +83,25 @@ void print_file(cat_args* cat) {
                     line = line ? (char*)realloc(line, len * sizeof(char)) :(char*)malloc(len * sizeof(char));
                     line[len - 1] = c;
                 } else {
-                    if (line) {
-                        line = (char*)realloc(line, (len + 1) * sizeof(char));
-                        line[len] = '\0';
-                        printf("%s", line);
-                        free(line);
-                        line = NULL;
-                    }
-                    if (c == '\n') printf("\n");
+                    apply_options(c, &line, len, cat, &right_after_blank);
                     len = 0;
                 }
             } while (c != EOF);
         } else printf("%s: No such file or directory\n", cat->files[i]);
+    }
+}
+
+void apply_options(char c, char** line, int len, cat_args* cat, bool* right_after_blank) {
+    if (*line) {
+        *line = (char*)realloc(*line, (len + 1) * sizeof(char));
+        (*line)[len] = '\0';
+        printf("%s", *line);
+        if (c == '\n') printf("\n");
+        free(*line);
+        *line = NULL;
+        *right_after_blank = false;
+    } else if (!cat->squeeze || !*right_after_blank) {
+        printf("\n");
+        *right_after_blank = true;
     }
 }
