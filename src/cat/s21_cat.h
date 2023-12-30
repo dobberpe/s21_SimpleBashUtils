fc204433e5f7d909ab58c2ad84a714c0ed42116b
Dobber Peas <dobberpe@le-c4.nov.21-school.ru>
2023-12-30 15:00:09 +0300

    no leaks, clang ok

--- src/cat/s21_cat.h
+++ src/cat/s21_cat.h
@@ -1,23 +1,24 @@
 #ifndef S21_CAT_H
 #define S21_CAT_H
 
+#include <getopt.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <getopt.h>
 
 typedef struct {
-    bool squeeze;
-    bool nonprint;
-    bool tab;
-    bool endl;
-    bool num_nonblank;
-    bool num;
+  bool squeeze;
+  bool nonprint;
+  bool tab;
+  bool endl;
+  bool num_nonblank;
+  bool num;
 } cat_args;
 
 cat_args* get_options(int argc, char** argv);
 cat_args* init_cat();
 void print_file(int argc, char** argv, cat_args* cat);
-void print_char(unsigned char c, cat_args* cat, int* line_counter, bool newline);
+void print_char(unsigned char c, cat_args* cat, int* line_counter,
+                bool newline);
 
 #endif
